728d47a111657d17a38aae61070b888cfc311b59
Igor Gunin <gunin-igor75@yandex.ru>
2022-09-05 17:57:35 +0600

    Upgrade Main add files

---
+++ src/Bicycle.java
@@ -0,0 +1,27 @@
+public class Bicycle extends ServiceStation{
+
+    private String modelName;
+    private int wheelsCount;
+
+    public Bicycle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+}
